f241e8bcbdae61e3254f2911d559e327e4c10047
ajroot5685 <anjeong8577@naver.com>
2024-04-17 12:11:49 +0900

    자동 업데이트 로직 수정

--- assets/js/pwa/sw.js
+++ assets/js/pwa/sw.js
@@ -1,13 +1,23 @@
-self.addEventListener('install', function(e) {
-    self.skipWaiting();
-  });
-  
-self.addEventListener('activate', function(e) {
-    self.registration.unregister()
-      .then(function() {
-        return self.clients.matchAll();
-      })
-      .then(function(clients) {
-        clients.forEach(client => client.navigate(client.url))
+self.addEventListener('install', function (e) {
+  self.skipWaiting();
+});
+self.addEventListener('activate', function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll();
+    })
+    .then(function (clients) {
+      clients.forEach((client) => client.navigate(client.url));
+      return Promise.resolve();
+    })
+    .then(() => {
+      self.caches.keys().then(function (cacheNames) {
+        Promise.all(
+          cacheNames.map(function (cacheName) {
+            return self.caches.delete(cacheName);
+          })
+        );
       });
+    });
 });
