53d65ce8a8f3b47cb6632d16fc329431323dc45b
ajroot5685 <anjeong8577@naver.com>
2024-04-17 12:51:33 +0900

    pwa 업데이트 메세지도 출력되지 않도록 수정

--- assets/js/pwa/app.js
+++ assets/js/pwa/app.js
@@ -8,41 +8,20 @@ if ('serviceWorker' in navigator) {
 
   if (isEnabled) {
     const swUrl = '{{ '/sw.min.js' | relative_url }}';
-    const $notification = $('#notification');
-    const $btnRefresh = $('#notification .toast-body>button');
 
     navigator.serviceWorker.register(swUrl).then((registration) => {
-      {% comment %}In case the user ignores the notification{% endcomment %}
-      if (registration.waiting) {
-        $notification.toast('show');
-      }
-
       registration.addEventListener('updatefound', () => {
-        registration.installing.addEventListener('statechange', () => {
-          if (registration.waiting) {
-            if (navigator.serviceWorker.controller) {
-              $notification.toast('show');
-            }
+        const installingWorker = registration.installing;
+        installingWorker.addEventListener('statechange', () => {
+          if (installingWorker.state === 'installed' && navigator.serviceWorker.controller) {
+            installingWorker.postMessage('SKIP_WAITING');
           }
         });
       });
-
-      $btnRefresh.on('click', () => {
-        if (registration.waiting) {
-          registration.waiting.postMessage('SKIP_WAITING');
-        }
-        $notification.toast('hide');
-      });
     });
 
-    let refreshing = false;
-
-    {% comment %}Detect controller change and refresh all the opened tabs{% endcomment %}
     navigator.serviceWorker.addEventListener('controllerchange', () => {
-      if (!refreshing) {
-        window.location.reload();
-        refreshing = true;
-      }
+      window.location.reload();
     });
   } else {
     navigator.serviceWorker.getRegistrations().then(function (registrations) {
